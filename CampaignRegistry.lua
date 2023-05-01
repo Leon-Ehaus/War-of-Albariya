68096f9b189563f8fd2463fcf219d81154108e4f
Leon Ehaus <leon.ehaus@t-online.de>
2023-05-01 17:43:46 +0200

    import obsidian notes

--- CampaignRegistry.lua
+++ CampaignRegistry.lua
@@ -3,6 +3,15 @@
 	["sidebarexpand"] = {
 		[1] = "tool",
 		[2] = "library",
+		[3] = "create",
+		[4] = "campaign",
+		[5] = "player",
+	},
+	["OptMANUALROLL"] = "on",
+	["windowstate"] = {
+		["tokenbag"] = {
+			["lastfilter"] = "image",
+		},
 	},
 	["sidebarversion"] = 2,
 }
